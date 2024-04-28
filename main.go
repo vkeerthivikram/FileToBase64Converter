226c72f342a3c48dfab44a3f20be74022e3efb94
vkeerthivikram <github@mail.vkvikram.com>
2024-04-28 17:40:55 +0530

    Refactor main.go and improve code clarity
    Removed unnecessary comments and simplified the ConvertFileToBase64 function in the main.go file. This refactor improves code readability and maintainability. The function now succinctly describes what it does, leading to better understanding for future development.

--- main.go
+++ main.go
@@ -11,7 +11,6 @@ import (
 
 func main() {
 	args := os.Args
-	// If no argument is given print an error and return
 	if len(args) != 2 {
 		fmt.Println("Please provide a file path")
 		return
@@ -20,6 +19,12 @@ func main() {
 	ConvertFileToBase64(args[1])
 }
 
+// ConvertFileToBase64 converts a file to its Base64 representation and saves it as a new file.
+// The function takes a file path as input and reads the file content into a byte array.
+// It then encodes the byte array to Base64 format and creates a new file with the encoded data.
+// The new file is saved in the same directory as the original file, with a name starting with "base64_",
+// followed by the original file name and a timestamp in Unix format.
+// The function returns an error if any of the file operations fail.
 func ConvertFileToBase64(fp string) {
 	file, err := os.Open(fp)
 
@@ -35,32 +40,21 @@ func ConvertFileToBase64(fp string) {
 	}(file)
 
 	fileInfo, _ := file.Stat()
-	var fileSize int64 = fileInfo.Size()
+	var fileSize = fileInfo.Size()
 	bytes := make([]byte, fileSize)
-
-	// Read file content to bytes
 	_, err = file.Read(bytes)
 	if err != nil {
 		log.Fatal(err)
 		return
 	}
-
-	// Encode to base64
 	encoded := base64.StdEncoding.EncodeToString(bytes)
-
-	// Create new file
 	timestamp := time.Now().Unix()
 	baseFileName := filepath.Base(fp)
 	newFileName := fmt.Sprintf("base64_%s_%d.txt", baseFileName, timestamp)
-
 	newFilePath := filepath.Join(filepath.Dir(fp), newFileName)
-
 	err = os.WriteFile(newFilePath, []byte(encoded), 0644)
-
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	fmt.Printf("File successfully encrypted to base64 and saved as %s\n", newFileName)
-
 }
